3b30f948b5102a1d2cfb57a6b66e0120e6535ba4
Tomao <2694235519@qq.com>
2022-11-06 17:31:37 +0800

    feat: 11.6

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use std::{io, cmp::Ordering};
+use std::{cmp::Ordering, io};
 
-use rand::{Rng};
+use rand::Rng;
 
 fn main() {
     println!("猜数游戏");
-    println!("输入猜的数字");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("无法读取行");
-    
     let scret_number = rand::thread_rng().gen_range(0, 101);
-    let guess:u32  =guess.trim().parse().expect("please type a number");
-    println!("你猜测的数字是 {}",guess);
-    
-    println!("神秘数字是 {}",scret_number);
+    loop {
+        println!("输入猜的数字");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+        println!("你猜测的数字是 {}", guess);
+        // match处理错误  
+        let guess: u32 =match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>{
+                println!("请输入数字");
+                continue;
+            }
+        };
         match guess.cmp(&scret_number) {
-            Ordering:: Less=>println!("Too small"),
-            Ordering::Greater=>println!("TOO big"),
-            Ordering::Equal=>println!("Equal")
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("TOO big"),
+            Ordering::Equal => println!("Equal"),
         }
+    }
+
+
+
+
+
+
 }
